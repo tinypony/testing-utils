f0909f976addfe45aab102bb9c4b4f5d6d0bb51e
Artur Latypov <tinypony@github.com>
2016-06-10 13:53:02 +0300

    added logging

--- metric-generator/generator.py
+++ metric-generator/generator.py
@@ -123,6 +123,7 @@ for dp in rate_limit(args, required_byte_rate):
 	millis = int(round(time() * 1000))
 	payload = dp.replace('sentat=0000000000000', 'sentat={}'.format(millis))
 	sock.sendto(payload, (HOST, PORT))
+	print 'Generated: {}'.format(payload)
 	bytes_sent += len(payload)
 
 end_time = time()
