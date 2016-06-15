471874b47e3144e7d2843c138069b46d3807ec5a
Artur Latypov <artur.latypov@comptel.com>
2016-06-15 08:20:06 +0000

    automated benchmark run

---
+++ metric-generator/run_benchmark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $1 ]; then 
+	echo "specify direct ip address"
+	exit
+fi
+python generator.py --rate 256 --number 10000 --direct True  --port 8089 --ip $1
+python generator.py --rate 512 --number 10000 --direct True  --port 8089 --ip $1
+python generator.py --rate 1024 --number 10000 --direct True  --port 8089 --ip $1
+
+python generator.py --rate 256 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
+python generator.py --rate 512 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
+python generator.py --rate 1024 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
