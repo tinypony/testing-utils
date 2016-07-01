b70d96e4906274b02c014530f881d848cee3110a
Artur Latypov <tinypony@github.com>
2016-07-01 11:50:21 +0300

    added protocol udp to direct connection

--- metric-generator/run_benchmark.sh
+++ metric-generator/run_benchmark.sh
@@ -3,9 +3,9 @@ if [ -z $1 ]; then
 	echo "specify direct ip address"
 	exit
 fi
-python generator.py --rate 256 --number 10000 --direct True  --port 8089 --ip $1
-python generator.py --rate 512 --number 10000 --direct True  --port 8089 --ip $1
-python generator.py --rate 1024 --number 10000 --direct True  --port 8089 --ip $1
+python generator.py --rate 256 --number 10000 --direct True --protocol udp  --port 8089 --ip $1
+python generator.py --rate 512 --number 10000 --direct True --protocol udp  --port 8089 --ip $1
+python generator.py --rate 1024 --number 10000 --direct True --protocol udp --port 8089 --ip $1
 
 python generator.py --rate 256 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
 python generator.py --rate 512 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
