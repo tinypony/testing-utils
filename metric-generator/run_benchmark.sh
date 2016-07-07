328c2a91d4db6e88f3fa03c30b86ed4f371a2656
Artur Latypov <tinypony@github.com>
2016-07-07 16:37:18 +0300

    added 2048 run

--- metric-generator/run_benchmark.sh
+++ metric-generator/run_benchmark.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-if [ -z $1 ]; then 
-	echo "specify direct ip address"
-	exit
+if [ -z $1 ]; then
+        echo "specify direct ip address"
+        exit
 fi
+
+if [ -z $2 ]; then
+        echo "specify cluster size"
+        exit
+fi
+
 python generator.py --rate 256 --number 10000 --direct True --protocol udp  --port 8089 --ip $1
 python generator.py --rate 512 --number 10000 --direct True --protocol udp  --port 8089 --ip $1
 python generator.py --rate 1024 --number 10000 --direct True --protocol udp --port 8089 --ip $1
+python generator.py --rate 2048 --number 10000 --direct True --protocol udp --port 8089 --ip $1
 
-python generator.py --rate 256 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
-python generator.py --rate 512 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
-python generator.py --rate 1024 --number 10000 --direct False  --port 9876 --ip 127.0.0.1
+python generator.py --rate 256 --number 10000 --direct False --cluster-size $2  --port 9876 --ip 127.0.0.1
+python generator.py --rate 512 --number 10000 --direct False --cluster-size $2  --port 9876 --ip 127.0.0.1
+python generator.py --rate 1024 --number 10000 --direct False --cluster-size $2  --port 9876 --ip 127.0.0.1
+python generator.py --rate 2048 --number 10000 --direct False --cluster-size $2  --port 9876 --ip 127.0.0.1
